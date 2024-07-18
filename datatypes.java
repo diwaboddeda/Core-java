ffa566ebdd77563783f6926050da81c1eb88deda
diwaboddeda <neibodd4492@gmail.com>
2024-07-18 22:54:08 +0530

    Datatypes

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+class datatypes
+{
+    public static void main(String[] args)
+     {
+       int i = 20;
+       short s = 558;
+       byte by = 127; 
+       long l = 1234l;
+       float f = 2.45f;
+       double d= 2.43;
+       char c = 'k';
+       boolean b= true; 
+
+       System.out.println(d);
+
+    }
+}
