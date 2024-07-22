971c8260b3c20f9159499f93b78338f24dab9e42
diwaboddeda <neibodd4492@gmail.com>
2024-07-23 01:15:44 +0530

    typeconversions

---
+++ typeconversion.java
@@ -0,0 +1,31 @@
+public class typeconversion {
+    public static void main(String[] args) {
+        
+        /*byte a=125;
+
+        int y =a;
+        //System.out.println(b);         //option+shift+down for copying same line 
+
+
+        
+
+        int x = 123;         
+        //byte z=x;              //we cannot asssign int to bye directly
+        byte z= (byte )x;       //inorder to assign int value to byte we need to do type casting by adding byte to the value
+        System.out.println(z);
+
+        float c =5.6f;
+        int t=  (int )c;
+        System.out.println(t);*/
+
+        byte h =10;
+        byte j=12;
+
+        int result = h*j;
+        System.out.println(result);
+
+
+
+    
+    }
+}
